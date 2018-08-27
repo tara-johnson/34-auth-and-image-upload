d851a2fb069cf78e97c01c7f84e61e41faf77f22
Tara Johnson <tara@madface.com>
2018-08-27 14:02:42 -0700

    Email auhtentication working.

--- lab-tara/app/src/main/java/com/example/photogram/LoginActivity.java
+++ lab-tara/app/src/main/java/com/example/photogram/LoginActivity.java
@@ -5,9 +5,12 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -20,12 +23,17 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LOGIN";
+
     private FirebaseAuth mAuth;
 
     @BindView(R.id.loggedInOptions) LinearLayout mLoggedInOptions;
     @BindView(R.id.loggedOutOptions) LinearLayout mLoggedOutOptions;
 
     @BindView(R.id.usernameInfo) TextView mUsernameInfo;
+    @BindView(R.id.usernameInput) TextView mEmail;
+    @BindView(R.id.passwordInput) TextView mPassword;
+    @BindView(R.id.emailLogin) Button mEmailLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,6 +78,29 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+    @OnClick(R.id.emailLogin)
+    public void login() {
+        String email = mEmail.getText().toString();
+        String password = mPassword.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "createUserWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    updateUI(user);
+                } else {
+                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                    Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                    updateUI(null);
+                }
+            }
+        });
+    }
+
+
 
     @OnClick(R.id.proceedToFeed)
     public void proceedToFeed() {
